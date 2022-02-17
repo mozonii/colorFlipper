396ddb01b247bd2dc51bc680ab160e6a6cab786f
rapha <ms.maina001@gmail.com>
2022-02-17 19:33:38 +0300

    Flipper done

---
+++ index.js
@@ -0,0 +1,18 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const button = document.querySelector(".btn");
+const color = document.querySelector(".color")
+
+button.addEventListener('click', function(event){
+    let hexColor = "#"
+
+    for( let i=0; i<6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexColor
+    color.innerText = hexColor
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length)
+}
